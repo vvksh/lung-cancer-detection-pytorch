4c3b377d5bae9b8f4d5a895695089cef61288c9f
Vivek Sah <vivek.sah32+github@gmail.com>
2021-02-16 23:18:01 -0800

    Implement datasets
    Fixes #1

---
+++ dsets.py
@@ -0,0 +1,180 @@
+import csv, copy
+import functools
+import glob
+import logging
+import os
+from collections import namedtuple
+import SimpleITK as sitk
+import numpy as np
+import torch as torch
+from torch.utils.data import Dataset
+
+from util.utils import xyz2irc, irc2xyz, XyzTuple, IrcTuple
+from util.disk import getCache
+
+raw_cache = getCache('luna_raw')
+
+CandidateInfoTuple = namedtuple(
+    'CandidateInfoTuple',
+    'isNodule_bool, diameter_mm, series_uid, center_xyz',
+)
+
+@functools.lru_cache(1)
+def get_candidate_info_list(requireOnDisk_bool = True):
+    mhd_list = glob.glob('data/subset*/*.mhd')
+    presentOnDisk_set = {os.path.split(p)[-1][:-4] for p in mhd_list}
+
+    diameter_dict = {}
+    with open('data/annotations.csv') as f:
+        for row in list(csv.reader(f))[1:]:
+            series_uid = row[0]
+            annotationCenter_xyz = tuple([float(x) for x in row[1:4]])
+            annotationCenter_mm = float(row[4])
+            diameter_dict.setdefault(series_uid, []).append((annotationCenter_xyz, annotationCenter_mm))
+
+    candidateInfo_list = []
+    with open('data/candidates.csv', "r") as f:
+        for row in list(csv.reader(f))[1:]:
+            series_uid = row[0]
+
+            if series_uid not in presentOnDisk_set and requireOnDisk_bool:
+                continue
+
+            isNodule_bool = bool(int(row[4]))
+            candidateCenter_xyz = tuple([float(x) for x in row[1:4]])
+
+            canditateDiamter_mm = 0.0
+            for annotation_tup in diameter_dict.get(series_uid, []):
+                annotationCenter_xyz, annotationDiameter_mm = annotation_tup
+                for i in range(3):
+                    delta_mm = abs(candidateCenter_xyz[i] - annotationCenter_xyz[i])
+                    if delta_mm > annotationDiameter_mm/4:
+                        break
+                else:
+                    canditateDiamter_mm = annotationDiameter_mm
+                    break
+            candidateInfo_list.append(CandidateInfoTuple(
+                isNodule_bool,
+                canditateDiamter_mm,
+                series_uid,
+                candidateCenter_xyz
+            ))
+    candidateInfo_list.sort(reverse=True)
+    return candidateInfo_list
+
+class Ct:
+    def __init__(self, series_uid):
+        mhd_path = glob.glob(f"data/subset*/{series_uid}.mhd")[0]
+        ct_mhd = sitk.ReadImage(mhd_path)
+        ct_a = np.array(sitk.GetArrayFromImage(ct_mhd), dtype=np.float32)
+        ct_a.clip(-1000, 1000, ct_a) # remove < -1000 (air) and over 1000 (bones, metals)
+        self.series_uid = series_uid
+        self.hu_a = ct_a
+        self.origin_xyz = XyzTuple(*ct_mhd.GetOrigin())
+        self.vxSize_xyz = XyzTuple(*ct_mhd.GetSpacing())
+        # Converts the directions to an array, and reshapes the nine-element array to its proper 3 × 3 matrix shape
+        self.direction_a = np.array(ct_mhd.GetDirection()).reshape(3,3)
+
+    def get_raw_candidate(self, center_xyz, width_irc):
+        center_irc = xyz2irc(
+            center_xyz,
+            self.origin_xyz,
+            self.vxSize_xyz,
+            self.direction_a
+        )
+
+        slice_list = []
+        for axis, center_val in enumerate(center_irc):
+            start_ndx = int(round(center_val - width_irc[axis]/2))
+            end_ndx = int(start_ndx + width_irc[axis])
+            assert center_val >= 0 and center_val < self.hu_a.shape[axis], repr(
+                [self.series_uid, center_xyz, self.origin_xyz, self.vxSize_xyz, center_irc, axis])
+
+            if start_ndx < 0:
+                # log.warning("Crop outside of CT array: {} {}, center:{} shape:{} width:{}".format(
+                #     self.series_uid, center_xyz, center_irc, self.hu_a.shape, width_irc))
+                start_ndx = 0
+                end_ndx = int(width_irc[axis])
+
+            if end_ndx > self.hu_a.shape[axis]:
+                # log.warning("Crop outside of CT array: {} {}, center:{} shape:{} width:{}".format(
+                #     self.series_uid, center_xyz, center_irc, self.hu_a.shape, width_irc))
+                end_ndx = self.hu_a.shape[axis]
+                start_ndx = int(self.hu_a.shape[axis] - width_irc[axis])
+
+
+            slice_list.append(slice(start_ndx, end_ndx))
+
+        ct_chunk = self.hu_a[tuple(slice_list)]
+        return ct_chunk, center_irc
+
+
+@functools.lru_cache(1, typed=True)
+def getCt(series_uid):
+    return Ct(series_uid)
+
+
+@raw_cache.memoize(typed=True)
+def getCtRawCandidate(series_uid, center_xyz, width_irc):
+    ct = getCt(series_uid)
+    ct_chunk, center_irc = ct.get_raw_candidate(center_xyz, width_irc)
+    return ct_chunk, center_irc
+
+
+class LunaDataset(Dataset):
+    def __init__(self,
+                 val_stride=0,
+                 isValSet_bool=None,
+                 series_uid=None,
+            ):
+        self.candidateInfo_list = copy.copy(get_candidate_info_list())
+
+        if series_uid:
+            self.candidateInfo_list = [
+                x for x in self.candidateInfo_list if x.series_uid == series_uid
+            ]
+
+        if isValSet_bool:
+            assert val_stride > 0, val_stride
+            self.candidateInfo_list = self.candidateInfo_list[::val_stride]
+            assert self.candidateInfo_list
+        elif val_stride > 0:
+            del self.candidateInfo_list[::val_stride]
+            assert self.candidateInfo_list
+
+        logging.info("{!r}: {} {} samples".format(
+            self,
+            len(self.candidateInfo_list),
+            "validation" if isValSet_bool else "training",
+        ))
+
+    def __len__(self):
+        return len(self.candidateInfo_list)
+
+    def __getitem__(self, ndx):
+        candidateInfo_tup = self.candidateInfo_list[ndx]
+        width_irc = (32, 48, 48)
+
+        candidate_a, center_irc = getCtRawCandidate(
+            candidateInfo_tup.series_uid,
+            candidateInfo_tup.center_xyz,
+            width_irc,
+        )
+
+        candidate_t = torch.from_numpy(candidate_a)
+        candidate_t = candidate_t.to(torch.float32)
+        candidate_t = candidate_t.unsqueeze(0) # .unsqueeze(0) adds the ‘Channel’ dimension.
+
+        pos_t = torch.tensor([
+                not candidateInfo_tup.isNodule_bool,
+                candidateInfo_tup.isNodule_bool
+            ],
+            dtype=torch.long,
+        )
+
+        return (
+            candidate_t,
+            pos_t,
+            candidateInfo_tup.series_uid,
+            torch.tensor(center_irc),
+        )
---
+++ test_dsets.py
@@ -0,0 +1,8 @@
+from dsets import get_candidate_info_list, LunaDataset
+
+def test_get_candidate_info_list():
+    candidates = get_candidate_info_list()
+
+def test_LunaDataSet():
+    print(LunaDataset()[0])
+
