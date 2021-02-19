45a47a673fe0baa0b310a4f714e3ba5bdfb03b66
Vivek Sah <vivek.sah32+github@gmail.com>
2021-02-18 23:36:33 -0800

    Add luna model
    See #2

--- model.py
+++ model.py
@@ -1,4 +1,50 @@
+import math
+
 from torch import nn
+
+class LunaModel(nn.Module):
+    def __init__(self, in_channels=1, conv_channels=8):
+        super().__init__()
+
+        #tail
+        # will shift and scale our input so that it has a mean of 0 and a standard deviation of 1.
+        self.tail_batchnorm = nn.BatchNorm3d(1)
+
+        # backbone
+        self.block1 = LunaBlock(in_channels, conv_channels)
+        self.block2 = LunaBlock(conv_channels, conv_channels*2)
+        self.block3 = LunaBlock(conv_channels*2, conv_channels*4)
+        self.block4 = LunaBlock(conv_channels*4, conv_channels*8)
+
+        # head
+        self.head_linear = nn.Linear(1152, 2)
+        self.head_softmax = nn.Softmax(dim=1)
+
+    def forward(self, input_batch):
+        bn_output = self.tail_batchnorm(input_batch)
+
+        block_out = self.block1(bn_output)
+        block_out = self.block2(block_out)
+        block_out = self.block3(block_out)
+        block_out = self.block4(block_out)
+
+        conv_flat = block_out.view(
+            block_out.size(0), # batch_size
+            -1
+        )
+
+        linear_output = self.head_linear(conv_flat)
+
+        return linear_output, self.head_softmax(linear_output)
+
+    def _init_weights(self):
+        for m in self.modules():
+            if type(m) in {nn.Linear, nn.Conv3d}:
+                nn.init.kaiming_normal_(m.weight.data, a=0, mode='fan_out', nonlinearity='relu',)
+                if m.bias is not None:
+                    fan_in, fan_out = nn.init._calculate_fan_in_and_fan_out(m.weight.data)
+                    bound = 1/ math.sqrt(fan_out)
+                    nn.init.normal_(m.bias, -bound, bound)
 
 
 class LunaBlock(nn.Module):
@@ -24,4 +70,3 @@ class LunaBlock(nn.Module):
         block_out = self.relu2(block_out)
 
         return self.maxpool(block_out)
-    
